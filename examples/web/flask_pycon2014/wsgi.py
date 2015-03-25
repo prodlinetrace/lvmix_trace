d7303b5ebfc34b0097954d227493b1e2599335a5
wilkpio <piotr.wilkosz@gmail.com>
2015-03-25 12:29:45 +0100

    *.pyc and examples cleanup

--- examples/web/flask_pycon2014/wsgi.py
+++
@@ -1,9 +0,0 @@
-def application(environ, start_response):
-    """WSGI Application"""
-    start_response('200 OK', [('Content-type','text/html')])
-    yield '<h2>WSGI environment variables</h2>\n<pre><code>'
-    sorted_envs = environ.keys()[:]
-    sorted_envs.sort()
-    for k in sorted_envs:
-        yield '{0:<24}: {1}\n'.format(k, environ[k])
-    yield '</code></pre>'
