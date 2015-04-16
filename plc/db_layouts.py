86f409a3a803d1a3a74c1a4bf7ad3ba6124fe5e4
wilkpio <Piotr.Wilkosz@gmail.com>
2015-04-16 03:26:38 +0200

    replace WORD by INT. use format instead of replace when working with strings.

--- plc/db_layouts.py
+++ plc/db_layouts.py
@@ -15,9 +15,9 @@ db3xxHead = """
 """
 
 db300Body = """
-46.0 """ + PC_READY_FLAG + """      BOOL        # PC_Ready flag. Monitored by PLC. PCL waits for True. PC sets to False when it starts processing. PC sets back to True once processing is finished.
-46.1 """ + PLC_MESSAGE_FLAG + """   BOOL        # PLC_Query bit - monitored by PC. PC reads status from database if set to True. Once PC finishes it sets it back to False.
-46.2 """ + PLC_SAVE_FLAG + """      BOOL        # PLC_Save bit - monitored by PC. PC saves status if set to True. Once PC finishes it sets it back to False.
+46.0   {flag_pc_ready}              BOOL        # PC_Ready flag. Monitored by PLC. PCL waits for True. PC sets to False when it starts processing. PC sets back to True once processing is finished.
+46.1   {flag_plc_message}           BOOL        # PLC_Query bit - monitored by PC. PC reads status from database if set to True. Once PC finishes it sets it back to False.
+46.2   {flag_plc_save}              BOOL        # PLC_Save bit - monitored by PC. PC saves status if set to True. Once PC finishes it sets it back to False.
 46.4   body.res_1                   BOOL
 46.5   body.res_2                   BOOL
 46.6   body.res_3                   BOOL
@@ -27,7 +27,7 @@ db300Body = """
 49.0   body.byte_res_3              BYTE
 50.0   body.station_number          BYTE        # station_number - used when reading or saving station status. Value set by PLC when reading/writing status to/from database.
 51.0   body.station_status          BYTE        # station_status - used when reading or saving station status. Value set by PLC when saving status. Value set by PC when reading status from database.
-"""
+""".format(flag_pc_ready=PC_READY_FLAG, flag_plc_message=PLC_MESSAGE_FLAG, flag_plc_save=PLC_SAVE_FLAG)
 
 db300Ctrl = """
 52.0    ctrl.plc.live                BOOL        # blinks every 300[ms]. Indicates that PLC is alive.
@@ -56,37 +56,37 @@ db3xxTrcTail = """
 """
 
 db3xxTrcTemplate = """
-0.0    body.trc.tmpl.__no__.PC_Ready           BOOL        # PC_Ready bit - currently ignored
-0.1    body.trc.tmpl.__no__.res_1              BOOL
-0.2    body.trc.tmpl.__no__.PLC_Save           BOOL        # PLC_Save bit - monitored by PC. PC start block processing if set to True. Once PC finishes it sets it to False.
-0.3    body.trc.tmpl.__no__.res_2              BOOL
-0.4    body.trc.tmpl.__no__.res_3              BOOL
-0.5    body.trc.tmpl.__no__.res_4              BOOL
-0.6    body.trc.tmpl.__no__.res_5              BOOL
-0.7    body.trc.tmpl.__no__.res_6              BOOL
-1.0    body.trc.tmpl.__no__.res_byte           BYTE
-2.0    body.trc.tmpl.__no__.operation_status   BYTE        # overall operation status. 1 - OK, 2 - NOK, 4 - Not present in this variant,  5 - next one OK, 6 - next one NOK
-3.0    body.trc.tmpl.__no__.res_byte_0         BYTE
-4.0    body.trc.tmpl.__no__.operation_type     INT         # individual operation type
+0.0    body.trc.tmpl.{number}.PC_Ready           BOOL        # PC_Ready bit - currently ignored
+0.1    body.trc.tmpl.{number}.res_1              BOOL
+0.2    body.trc.tmpl.{number}.PLC_Save           BOOL        # PLC_Save bit - monitored by PC. PC start block processing if set to True. Once PC finishes it sets it to False.
+0.3    body.trc.tmpl.{number}.res_2              BOOL
+0.4    body.trc.tmpl.{number}.res_3              BOOL
+0.5    body.trc.tmpl.{number}.res_4              BOOL
+0.6    body.trc.tmpl.{number}.res_5              BOOL
+0.7    body.trc.tmpl.{number}.res_6              BOOL
+1.0    body.trc.tmpl.{number}.res_byte           BYTE
+2.0    body.trc.tmpl.{number}.operation_status   BYTE        # overall operation status. 1 - OK, 2 - NOK, 4 - Not present in this variant,  5 - next one OK, 6 - next one NOK
+3.0    body.trc.tmpl.{number}.res_byte_0         BYTE
+4.0    body.trc.tmpl.{number}.operation_type     INT         # individual operation type
 
-6.0    body.trc.tmpl.__no__.1.result           REAL        # operation #1 - measured result.
-10.0   body.trc.tmpl.__no__.1.result_max       REAL        # operation #1 - maximum value
-14.0   body.trc.tmpl.__no__.1.result_min       REAL        # operation #1 - minimum value
-18.0   body.trc.tmpl.__no__.1.result_status    INT         # operation #1 - status
-20.0   body.trc.tmpl.__no__.1.word_res         WORD        # not used - TODO: could be used as indication flag
+6.0    body.trc.tmpl.{number}.1.result           REAL        # operation #1 - measured result.
+10.0   body.trc.tmpl.{number}.1.result_max       REAL        # operation #1 - maximum value
+14.0   body.trc.tmpl.{number}.1.result_min       REAL        # operation #1 - minimum value
+18.0   body.trc.tmpl.{number}.1.result_status    INT         # operation #1 - status
+20.0   body.trc.tmpl.{number}.1.word_res         INT         # not used - TODO: could be used as indication flag
 
-22.0   body.trc.tmpl.__no__.2.result           REAL        # operation #2 - measured result.
-26.0   body.trc.tmpl.__no__.2.result_max       REAL        # operation #2 - maximum value
-30.0   body.trc.tmpl.__no__.2.result_min       REAL        # operation #2 - minimum value
-34.0   body.trc.tmpl.__no__.2.result_status    INT         # operation #2 - status
-36.0   body.trc.tmpl.__no__.2.word_res         WORD
+22.0   body.trc.tmpl.{number}.2.result           REAL        # operation #2 - measured result.
+26.0   body.trc.tmpl.{number}.2.result_max       REAL        # operation #2 - maximum value
+30.0   body.trc.tmpl.{number}.2.result_min       REAL        # operation #2 - minimum value
+34.0   body.trc.tmpl.{number}.2.result_status    INT         # operation #2 - status
+36.0   body.trc.tmpl.{number}.2.word_res         INT
 
-38.0   body.trc.tmpl.__no__.3.result           REAL        # operation #3 - measured result.
-42.0   body.trc.tmpl.__no__.3.result_max       REAL        # operation #3 - maximum value
-46.0   body.trc.tmpl.__no__.3.result_min       REAL        # operation #3 - minimum value
-50.0   body.trc.tmpl.__no__.3.result_status    INT         # operation #3 - status
-52.0   body.trc.tmpl.__no__.3.word_res         WORD
-54.0   body.trc.tmpl.__no__.date_time          DATETIME    # date and time - size is 8 bytes
+38.0   body.trc.tmpl.{number}.3.result           REAL        # operation #3 - measured result.
+42.0   body.trc.tmpl.{number}.3.result_max       REAL        # operation #3 - maximum value
+46.0   body.trc.tmpl.{number}.3.result_min       REAL        # operation #3 - minimum value
+50.0   body.trc.tmpl.{number}.3.result_status    INT         # operation #3 - status
+52.0   body.trc.tmpl.{number}.3.word_res         INT
+54.0   body.trc.tmpl.{number}.date_time          DATETIME    # date and time - size is 8 bytes
 # traceability template size is 62
 """
 
@@ -97,7 +97,7 @@ for i in range(0, 3):  # append 3 traceability templates.
     base_offset = 48
     block_size = 62
     offset = base_offset + block_size * i
-    db301 += offset_spec_block(db3xxTrcTemplate, offset).replace("__no__", str(i))
+    db301 += offset_spec_block(db3xxTrcTemplate, offset).replace("{number}", str(i))
 
 
 db302 = db3xxHead
@@ -106,7 +106,7 @@ for i in range(0, 1):  # append 1 traceability template.
     base_offset = 48
     block_size = 62
     offset = base_offset + block_size * i
-    db302 += offset_spec_block(db3xxTrcTemplate, offset).replace("__no__", str(i))
+    db302 += offset_spec_block(db3xxTrcTemplate, offset).format(number=str(i))
 
 db303 = db3xxHead
 db303 += offset_spec_block(db3xxTrcHead, 46)
@@ -114,7 +114,7 @@ for i in range(0, 4):  # append 4 traceability templates.
     base_offset = 48
     block_size = 62
     offset = base_offset + block_size * i
-    db303 += offset_spec_block(db3xxTrcTemplate, offset).replace("__no__", str(i))
+    db303 += offset_spec_block(db3xxTrcTemplate, offset).format(number=str(i))
 
 db304 = db3xxHead
 db304 += offset_spec_block(db3xxTrcHead, 46)
@@ -122,7 +122,7 @@ for i in range(0, 5):  # append 5 traceability templates.
     base_offset = 48
     block_size = 62
     offset = base_offset + block_size * i
-    db304 += offset_spec_block(db3xxTrcTemplate, offset).replace("__no__", str(i))
+    db304 += offset_spec_block(db3xxTrcTemplate, offset).format(number=str(i))
 
 
 # special cases hacking...
