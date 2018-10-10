7be3b323f6e2b6afc192c290d54462ce5c3c07fb
Piotr Wilkosz <piotr.wilkosz@gmail.com>
2018-10-10 06:36:29 +0200

    rebase and merge

--- traceability/__init__.py
+++ traceability/__init__.py
@@ -1,7 +1,7 @@
 """
 The PLC Python library.
 """
-__version__ = '0.7.2'
+__version__ = '0.8.0'
 AUTHOR = "Piotr Wilkosz"
 EMAIL = "Piotr.Wilkosz@gmail.com"
 NAME = "ProdLineTrace"
@@ -12,22 +12,23 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from .helpers import parse_config, parse_args
-from .constants import SQLALCHEMY_DATABASE_URI_PREFIX
 
 logger = logging.getLogger(__package__.ljust(12)[:12])
 
 _opts, _args = parse_args()
 _config = {}
-try:
-    _config['dbfile'] = parse_config(_opts.config)['main']['dbfile'][0]
-except Exception, e:
-    _config['dbfile'] = tempfile.gettempdir() + os.sep + 'plc_temp.sqlite'
 
-db_connection_string = SQLALCHEMY_DATABASE_URI_PREFIX + _config['dbfile']
+try:
+    _config['dburi'] = parse_config(_opts.config)['main']['dburi'][0]
+except Exception, e:
+    _config['dburi'] = 'sqlite:///' + tempfile.gettempdir() + os.sep + NAME + '.sqlite'
+    
+if 'DATABASE_URL' in os.environ: 
+    _config['dburi'] = os.environ.get('DATABASE_URL')
 
 _app = Flask(__name__)
-_app.config['SQLALCHEMY_DATABASE_URI'] = db_connection_string
+_app.config['SQLALCHEMY_DATABASE_URI'] = _config['dburi']
 db = SQLAlchemy(_app)
 
-if not _config['dbfile'].endswith("plc_temp.sqlite"):
-    db.create_all()
+# try to create schema
+db.create_all()
